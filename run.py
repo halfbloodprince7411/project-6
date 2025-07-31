43dc40b9235a904c15e321c753f86c0361c4543e
siddharth7411 <siddharth7411@gmail.com>
2025-07-31 18:05:41 -0400

    just work

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from app import create_app
+from flask_talisman import Talisman
+import os
 
 app = create_app()
 
+# Only enforce HTTPS in production
+if os.getenv("AZURE_ENV") == "PRODUCTION":
+    Talisman(app, content_security_policy=None)
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
-
