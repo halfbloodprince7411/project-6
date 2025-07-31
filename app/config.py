b22414bb23f671da88b133e6f60f682cf25f9ccc
siddharth7411 <siddharth7411@gmail.com>
2025-07-31 17:42:09 -0400

    more changes12

--- app/config.py
+++ app/config.py
@@ -11,6 +11,7 @@ TEACHERS_GROUP_ID = get_secret("teachers-group-id")
 if os.getenv("AZURE_ENV") == "PRODUCTION":
     SERVER_NAME = "ncpl-training-app-47fb6d.azurewebsites.net"
     REDIRECT_PATH = "/authorized"
+    AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 else:
     SERVER_NAME = "localhost:5001"
     REDIRECT_PATH = "/authorized"
