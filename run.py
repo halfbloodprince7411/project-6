1418e6867e9f0beab267020b8b458235d2c5e664
siddharth7411 <siddharth7411@gmail.com>
2025-07-31 18:15:47 -0400

    just work1

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = create_app()
 
 # Only enforce HTTPS in production
 if os.getenv("AZURE_ENV") == "PRODUCTION":
-    Talisman(app, content_security_policy=None)
+    Talisman(app, force_https=True, content_security_policy=None)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
